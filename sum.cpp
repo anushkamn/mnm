73ea1a394e8d4cebdeb15b6d47ae01379b6d62b5
anushka <anushkanagpal75@gmail.com>
2018-10-30 23:53:32 +0530

    added a new file

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x,y,sum;
+cout<<"Enter first number : ";
+cin>>x;
+cout<<"Enter second number : ";
+cin>>y;	
+cout<<"sum is "	;
+sum=x+y; 
+cout<<sum;
+return 0;
+}
